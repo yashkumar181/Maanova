825160bc752bf261789ac2b03580f586c52d78fb
yashkumar181 <yashkumar18ch@gmail.com>
2025-09-10 22:34:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Mental-Health-Platform',
+  description: 'Created by commitMints',
+  generator: 'commitMints',
 }
 
 export default function RootLayout({

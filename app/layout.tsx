6a09f2d5aa40968cb34367a40301db64e0bf7dfb
yashkumar181 <yashkumar18ch@gmail.com>
2025-09-11 01:48:18 +0530

    botpress

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
+        <script src="https://cdn.botpress.cloud/webchat/v3.2/inject.js" defer></script>
+        <script src="https://files.bpcontent.cloud/2025/09/09/10/20250909105231-ITKTK13R.js" defer></script>
       </body>
     </html>
   )

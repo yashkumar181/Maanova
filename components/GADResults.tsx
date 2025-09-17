3f0636d408bdf3468c0e25e9a77f1522fb27d558
yashkumar181 <yashkumar18ch@gmail.com>
2025-09-17 19:45:29 +0530

    Update GADResults.tsx

--- components/GADResults.tsx
+++ components/GADResults.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';

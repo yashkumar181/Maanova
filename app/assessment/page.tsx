ed4fa83b200cd0a636a6c6c96f3313b3d65bad4e
yashkumar181 <yashkumar18ch@gmail.com>
2025-09-18 20:20:58 +0530

    Update page.tsx

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -1,5 +1,6 @@
 // In app/resources/who5-assessment/page.tsx
 "use client";
+
 import { useState,useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
